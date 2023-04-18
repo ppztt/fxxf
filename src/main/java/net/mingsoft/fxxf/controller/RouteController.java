5a817f69735028478643a5aaa368df4d43891ee8
ni <123@.com>
2023-04-18 10:30:03 +0800

    feat:菜单页面跳转controller

---
+++ src/main/java/net/mingsoft/fxxf/controller/RouteController.java
@@ -0,0 +1,28 @@
+package net.mingsoft.fxxf.controller;
+
+import io.swagger.annotations.Api;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * 菜单页面路由跳转控制器
+ */
+@Api(tags = {"菜单页面路由跳转控制器"})
+@Controller("routeController")
+@RequestMapping("/${ms.manager.path}/route")
+@Slf4j
+@Scope("request")
+public class RouteController {
+
+//    /**
+//     * 监督投诉统计菜单跳转
+//     */
+//    @GetMapping("/feedback/statistic")
+//    public String index() {
+//        return "/fxxf/feedback/index";
+//    }
+
+}
