c4af8ec324d4fc683427a41068cd0ea2ce7ca8e8
luwb <1571701516@qq.com>
2023-04-23 10:27:00 +0800

    商品和服务类别统计接口优化

--- src/main/java/net/mingsoft/fxxf/service/impl/GoodsAndServicesStatServiceImpl.java
+++ src/main/java/net/mingsoft/fxxf/service/impl/GoodsAndServicesStatServiceImpl.java
@@ -1,16 +1,26 @@
 package net.mingsoft.fxxf.service.impl;
 
 
+import cn.hutool.core.date.DateTime;
+import cn.hutool.core.date.DateUtil;
+import cn.hutool.core.text.CharSequenceUtil;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import net.mingsoft.fxxf.bean.entity.Applicants;
+import net.mingsoft.fxxf.bean.entity.Type;
 import net.mingsoft.fxxf.bean.vo.GoodsAndServicesStat;
-import net.mingsoft.fxxf.mapper.GoodsAndServicesStatMapper;
+import net.mingsoft.fxxf.service.ApplicantsService;
 import net.mingsoft.fxxf.service.GoodsAndServicesStatService;
+import net.mingsoft.fxxf.service.TypeService;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
-import java.util.Map;
+import java.util.Objects;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * 商品服务类型统计
@@ -19,75 +29,94 @@ import java.util.Map;
 public class GoodsAndServicesStatServiceImpl implements GoodsAndServicesStatService {
 
     @Resource
-    private GoodsAndServicesStatMapper goodsAndServicesStatMapper;
+    private ApplicantsService applicantsService;
+    @Resource
+    private TypeService typeService;
 
     public ArrayList<GoodsAndServicesStat> list(String city, String district, String startTime, String endTime) {
-
         ArrayList<GoodsAndServicesStat> result = new ArrayList<>();
-        List<Map> list = goodsAndServicesStatMapper.list2(city, district, startTime, endTime);
+        List<Type> typeList = getTypeList();
+        List<Applicants> applicantsList = getapplicantsList(city, district, startTime, endTime);
+        for (Type type : typeList) {
+            Integer typeId = type.getId();
+            String typeName = type.getName();
 
-        List<Map> listType = goodsAndServicesStatMapper.listType();
-
-        boolean isDistrict = StringUtils.isNotBlank(district);
-
-        for (int i = 0; i < listType.size(); i++) {
-            Map map = listType.get(i);
-            String servicetype = map.get("servicetype").toString();
-            String id = map.get("id").toString();
-            String typename = map.get("typename").toString();
-
-            int fxxf = 0;
-            int wlyxf = 0;
-            for (int j = 0; j < list.size(); j++) {
-                Map listmap = list.get(j);
-                Object typenameList = listmap.get("typename");
-                if (typenameList != null && typenameList.equals(typename)) {
-
-                    if (isDistrict) {
-                        String districts = listmap.get("district").toString();
-                        String[] c = districts.split(",");
-                        Integer type = Integer.parseInt(listmap.get("type").toString());
-                        if (type == 1) {
-                            fxxf += getStringToArrayCount(district, c);
-                        } else if (type == 2) {
-                            wlyxf += getStringToArrayCount(district, c);
-                        }
-                    } else {
-                        String citys = listmap.get("city").toString();
-                        String[] c = citys.split(",");
-                        Integer type = Integer.parseInt(listmap.get("type").toString());
-                        if (type == 1) {
-                            fxxf += getStringToArrayCount(city, c);
-                        } else if (type == 2) {
-                            wlyxf += getStringToArrayCount(city, c);
-                        }
+            AtomicInteger fxxf = new AtomicInteger(0);
+            AtomicInteger wlyxf = new AtomicInteger(0);
+            for (Applicants applicants : applicantsList) {
+                Integer applicantsType = applicants.getType();
+                String applicantsCity = applicants.getCity();
+                String applicantsDistrict = applicants.getDistrict();
+                String details = applicants.getDetails();
+                String[] detailArray = details.split(",");
+                for (String detailItem : detailArray) {
+                    if (!Objects.equals(detailItem, typeName)) {
+                        continue;
                     }
-
+                    if (StringUtils.isNotBlank(district)) {
+                        // 按区县统计
+                        statistic(fxxf, wlyxf, applicantsType, district, applicantsDistrict);
+                    } else {
+                        // 按市统计
+                        statistic(fxxf, wlyxf, applicantsType, city, applicantsCity);
+                    }
                 }
             }
-
             GoodsAndServicesStat goodsAndServicesStat = new GoodsAndServicesStat();
-            goodsAndServicesStat.setFxxf(fxxf);
-            goodsAndServicesStat.setWlyxf(wlyxf);
-            goodsAndServicesStat.setServicetype(servicetype);
-            goodsAndServicesStat.setTypename(typename);
-            goodsAndServicesStat.setSid(Integer.parseInt(id));
+            goodsAndServicesStat.setFxxf(fxxf.get());
+            goodsAndServicesStat.setWlyxf(wlyxf.get());
+            goodsAndServicesStat.setServicetype(type.getRemarks());
+            goodsAndServicesStat.setTypename(typeName);
+            goodsAndServicesStat.setSid(typeId);
             result.add(goodsAndServicesStat);
         }
-
-        //return goodsAndServicesStatMapper.list(city, district, startTime, endTime);
         return result;
     }
 
-    private static int getStringToArrayCount(String srt, String[] arrary) {
-        int count = 0;
+    private void statistic(AtomicInteger fxxf, AtomicInteger wlyxf, Integer applicantsType, String region, String applicantsRegion) {
+        String[] regions = applicantsRegion.split(",");
+        int count = getStringToArrayCount(region, regions);
+        if (applicantsType == 1) {
+            fxxf.addAndGet(count);
+        }
+        if (applicantsType == 2) {
+            wlyxf.addAndGet(count);
+        }
+    }
 
-        for (int i = 0; i < arrary.length; i++) {
-            if (arrary[i].contains(srt)) {
+    private int getStringToArrayCount(String srt, String[] arrary) {
+        int count = 0;
+        for (String s : arrary) {
+            if (s.contains(srt)) {
                 count++;
             }
         }
         return count;
+    }
+
+    private List<Type> getTypeList() {
+        return typeService.list(new QueryWrapper<Type>().lambda()
+                .select(Type::getId, Type::getRemarks, Type::getName)
+                .in(Type::getType, Arrays.asList(5, 6))
+                .isNotNull(Type::getRemarks)
+                .isNotNull(Type::getName));
+    }
+
+    private List<Applicants> getapplicantsList(String city, String district, String startTimeStr, String endTimeStr) {
+        LambdaQueryWrapper<Applicants> queryWrapper = new QueryWrapper<Applicants>().lambda()
+                .select(Applicants::getType, Applicants::getCity, Applicants::getDistrict, Applicants::getManagement, Applicants::getDetails)
+                .isNotNull(Applicants::getDetails)
+                .like(CharSequenceUtil.isNotBlank(city), Applicants::getCity, city)
+                .like(CharSequenceUtil.isNotBlank(district), Applicants::getDistrict, district);
+        if (CharSequenceUtil.isNotBlank(startTimeStr)) {
+            DateTime startTime = DateUtil.beginOfDay(DateUtil.parseDate(startTimeStr));
+            queryWrapper.ge(Applicants::getStartTime, startTime);
+        }
+        if (CharSequenceUtil.isNotBlank(endTimeStr)) {
+            DateTime endTime = DateUtil.endOfDay(DateUtil.parseDate(endTimeStr));
+            queryWrapper.le(Applicants::getStartTime, endTime);
+        }
+        return applicantsService.list(queryWrapper);
     }
 
 }
